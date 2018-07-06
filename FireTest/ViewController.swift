de4de1d7043ec3fc705f5118a77e51fc641414b0
Santiago A suarez R <juarez955@hotmail.com>
2018-07-06 11:40:16 -0500

    Initial Commit

---
+++ FireTest/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  FireTest
+//
+//  Created by Santiago A suarez R on 7/6/18.
+//  Copyright © 2018 Krakenn. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
